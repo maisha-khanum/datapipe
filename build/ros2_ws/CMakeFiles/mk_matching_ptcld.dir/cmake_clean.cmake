675fb97ab53150a0314527c12987f4cd732f9df8
Maisha <mkhanummaisha@gmail.com>
2025-03-03 21:00:01 -0800

    MK_matching_ptcld checkpoint, switching depth to pc subscriber

---
+++ build/ros2_ws/CMakeFiles/mk_matching_ptcld.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mk_matching_ptcld.dir/src/MK_matching_ptcld.cpp.o"
+  "CMakeFiles/mk_matching_ptcld.dir/src/MK_matching_ptcld.cpp.o.d"
+  "mk_matching_ptcld"
+  "mk_matching_ptcld.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mk_matching_ptcld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
